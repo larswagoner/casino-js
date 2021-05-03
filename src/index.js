dc0621e3d9dbb721667cea956810266dab5fd032
maxgallup <m.gallup@student.vu.nl>
2021-05-03 10:07:18 +0200

    cleaned up, reinstalled create react app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hand from './components/hand';
-
-Hand.card
 
 ReactDOM.render(
   <React.StrictMode>
