52aede4f62f6a837d59d0d4d57fa5905bb8828b5
maxgallup <m.gallup@student.vu.nl>
2021-05-03 11:59:09 +0200

    added screens

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// import screens
+import Start from './screens/Start'
+import Main from './screens/Main'
+import Join from './screens/Join'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Join />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
